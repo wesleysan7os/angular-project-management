cdb4b837386a3a0e5958b22e29d4ade58aa822e4
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-12-11 17:28:07 -0300

    adding a site header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   imports: [BrowserModule],
@@ -11,6 +12,7 @@ import { EventsListComponent } from './events/events-list.component';
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    NavBarComponent
   ],
   bootstrap: [EventsAppComponent],
 })
