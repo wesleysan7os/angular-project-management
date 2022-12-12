8f2e89f104f0008b95ff93bf8abedf4595376a60
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-12-11 22:23:59 -0300

    repeating data with ngFor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -10,14 +10,17 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
       <div>Price: \${{ event.price }}</div>
       <div>
         <span>Location: {{ event.location.address }}</span>
-        <span class="pad-left">{{ event.location.city }}, {{ event.location.country }}</span>
+        <span class="pad-left"
+          >{{ event.location.city }}, {{ event.location.country }}</span
+        >
       </div>
     </div>
   `,
   styles: [`
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
-  `]
+    .thumbnail { min-height: 210px; }
+  `],
 })
 export class EventThumbnailComponent {
   @Input() event: any;
