272cde6dca04757cc478026283f8d541c5d139b8
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-12-13 17:22:45 -0300

    navigating from code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { ToastrService } from './common/toastr.service';
 
 import { EventsAppComponent } from './events-app.component';
+import { CreateEventComponent } from './events/create-event.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
@@ -22,7 +23,8 @@ import { appRoutes } from './routes';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
-    EventDetailsComponent
+    EventDetailsComponent,
+    CreateEventComponent
   ],
   providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent],
