a0aca69556cb62dd0a0803d19bc7e7fa353d5c50
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-12-12 22:53:00 -0300

    hiding and showing content with ngSwitch

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -7,14 +7,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
       <h2>{{ event?.name }}</h2>
       <div>Date: {{ event?.date }}</div>
       <div>Time: {{ event?.time }}</div>
-      <div>Price: \${{ event?.price }}</div>
-      <div [hidden]="!event?.location">
+      <div [ngSwitch]="event?.time">
+        Price: \${{ event?.price }}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div *ngIf="!!event?.location">
         <span>Location: {{ event?.location?.address }}</span>
         <span class="pad-left">
           {{ event?.location?.city }}, {{ event?.location?.country }}
         </span>
       </div>
-      <div [hidden]="!event?.onlineUrl">
+      <div *ngIf="!!event?.onlineUrl">
         <span>Online URL:{{ event?.onlineUrl }}</span>
       </div>
     </div>
