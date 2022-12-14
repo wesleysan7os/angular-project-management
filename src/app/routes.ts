421074b2b621a1bf07ad6a2fb08656c3f9ba0d27
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-12-13 22:02:34 -0300

    lazily loading feature modules

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -11,5 +11,10 @@ export const appRoutes: Routes = [
   { path: 'events', component: EventsListComponent, resolve: {events: EventListResolver} },
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
   { path: '404', component: Error404Component },
-  { path: '', redirectTo: '/events', pathMatch: 'full' }
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module')
+      .then(m => m.UserModule)
+  }
 ];
