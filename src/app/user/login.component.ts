06d169b864dd7b0ce457723beb2ee95fa52a2cf8
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-12-15 15:43:06 -0300

    validating template-based form controls

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [
+    `
+      em {
+        float: right;
+        color: #e05c65;
+        padding-left: 10px;
+      }
+    `,
+  ],
 })
 export class LoginComponent {
   userName: string;
   password: string;
+  mouseoverLogin: boolean;
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -18,5 +29,12 @@ export class LoginComponent {
 
   cancel(): void {
     this.router.navigate(['/events']);
+  }
+
+  showWarningMessage(formControl: AbstractControl): boolean {
+    if (formControl && formControl.invalid) {
+      return (formControl.touched || this.mouseoverLogin);
+    }
+    return false;
   }
 }
