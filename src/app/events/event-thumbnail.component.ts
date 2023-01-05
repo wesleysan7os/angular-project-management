4a7df0080bc2d105a6f62b26b1760b806caf155e
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2023-01-04 23:09:20 -0300

    using built-in pipes to format events data

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,11 +5,11 @@ import { Event } from './shared';
   selector: 'event-thumbnail',
   template: `
     <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
-      <h2>{{ event?.name }}</h2>
-      <div>Date: {{ event?.date }}</div>
+      <h2>{{ event?.name | uppercase }}</h2>
+      <div>Date: {{ event?.date | date:"shortDate" }}</div>
       <div>Time: {{ event?.time }}</div>
       <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
-        Price: \${{ event?.price }}
+        Price: {{ event?.price | currency:'USD' }}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
         <span *ngSwitchDefault>(Normal Start)</span>
