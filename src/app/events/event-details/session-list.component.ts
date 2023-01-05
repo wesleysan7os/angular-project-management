4f1bcab49574cbe4775e64cb9abf41890eec421d
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2023-01-05 11:27:27 -0300

    filtering sessions by level

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,10 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { Session } from '../shared';
 
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html',
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: Session[];
+  @Input() filterBy: string;
+  visibleSessions: Session[] = [];
+
+  ngOnChanges(): void {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filterBy: string): void {
+    if (filterBy === 'all') {
+      this.visibleSessions = [...this.sessions];
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filterBy;
+      });
+    }
+  }
 }
